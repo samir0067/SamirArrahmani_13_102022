7dbed5f6213c2680942c79a2f85b6a2ae25698d2
samir0067 <arrahmani.samir@gmail.com>
2022-11-04 08:29:47 +0100

    wip InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,7 +4,8 @@ type InputFieldProps = {
   label: string;
   id: string;
   type: string;
-  value: string;
+  value?: string;
+  checked?: boolean;
   onChange: ChangeEventHandler<HTMLInputElement>;
 };
 
@@ -13,12 +14,20 @@ const InputField: FC<InputFieldProps> = ({
   id,
   type,
   value,
+  checked,
   onChange,
 }: InputFieldProps) => {
   return (
-    <div className="input-wrapper">
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} value={value} onChange={onChange} />
+    <div className={type === "checkbox" ? "input-remember" : "input-wrapper"}>
+      {type !== "checkbox" && <label htmlFor={id}>{label}</label>}
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        checked={checked}
+      />
+      {type === "checkbox" && <label htmlFor={id}>{label}</label>}
     </div>
   );
 };
