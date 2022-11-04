d48f1234c2439dff59647644f9892e609c47e648
samir0067 <arrahmani.samir@gmail.com>
2022-11-04 08:28:46 +0100

    wip

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: FC = () => {
       <section className="hero_content">
         <h2 className="sr-only">Promoted Content</h2>
         <p className="hero_content_subtitle">No fees.</p>
-        <p className="hero__content_subtitle">No minimum deposit.</p>
+        <p className="hero_content_subtitle">No minimum deposit.</p>
         <p className="hero_content_subtitle">High interest rates.</p>
         <p className="hero_content_text">
           Open a savings account with Argent Bank today!
