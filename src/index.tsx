3c837c60a22de8f030e31e849986be3cf27359a3
samir0067 <arrahmani.samir@gmail.com>
2022-11-23 10:37:40 +0100

    wip remove persiste state

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import storeConfig, { storePersist } from "store/storeConfig";
-import { PersistGate } from "redux-persist/integration/react";
+import storeConfig from "store/storeConfig";
 import "normalize.css";
 import "index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={storeConfig}>
-      <PersistGate loading={null} persistor={storePersist}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
