e8d8426d4ac3c85011eeaae30b6c60871c75e7ab
samir0067 <arrahmani.samir@gmail.com>
2022-11-23 10:41:49 +0100

    wip reducer

---
+++ src/utils/validation.ts
@@ -0,0 +1,20 @@
+import { object, string } from "yup";
+
+export const errorsMessage = {
+  requiredMsg: "Please complete this field",
+  requiredPassword: "Please enter a password",
+  minPassword: "Field must be at least 6 characters",
+};
+
+export const signInSchema = object().shape({
+  email: string().required(errorsMessage.requiredMsg).trim(),
+  password: string()
+    .min(6, errorsMessage.minPassword)
+    .required(errorsMessage.requiredPassword)
+    .trim(),
+});
+
+export const profileSchema = object().shape({
+  firstName: string().required(errorsMessage.requiredMsg).trim(),
+  lastName: string().required(errorsMessage.requiredMsg).trim(),
+});
