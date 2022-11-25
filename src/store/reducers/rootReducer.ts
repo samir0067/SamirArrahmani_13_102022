876b7945ce8571d8def8bca6ea627da8f640f5a8
samir0067 <arrahmani.samir@gmail.com>
2022-11-25 10:41:42 +0100

    wip clean store

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import signIn from "store/reducers/signInReducer";
-import account from "store/reducers/accountReducer";
 
 export const rootReducer = combineReducers({
   signIn,
-  account,
 });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -7,22 +7,3 @@ export type SignInType = {
   lastName?: string;
   rememberMe?: boolean;
 };
-
-export type AccountType = {
-  accountId?: string;
-  name?: string;
-  sold?: number;
-  status?: string;
-  id?: string;
-};
-
-// TODO a fixer
-// export type ProfileType = {
-//   email?: string;
-//   password?: string;
-//   firstName?: string;
-//   lastName?: string;
-//   createdAt?: Date;
-//   updatedAt?: Date;
-//   id?: string;
-// };
